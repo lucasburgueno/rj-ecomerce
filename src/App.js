3ab6dd5965b9dd52ce3a3a472c4ab35c88d7c7f7
lucas burgueño <lucas_burgueno6697@hotmail.com>
2022-08-22 23:54:36 -0300

    agregando navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import { Header } from "./components/Header";
 
-function App() {
+const App = ()=>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1> </h1>
+        <nav>
+
+        </nav>
       </header>
+      <div className="nav">
+        <Header/>
+
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
