9e9f16704731962813ed2afe1a278215bb51dda4
lucas burgueño <lucas_burgueno6697@hotmail.com>
2022-08-23 13:13:07 -0300

    agregando bootstrap

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
-import "./App.css"
-import { Header } from "./components/Header";
+import "./App.css";
+import Header from "./components/Header";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const App = ()=>{
   return (
     <div>
-      <header>
-        <h1> </h1>
-        <nav>
-
-        </nav>
-      </header>
-      <div className="nav">
-        <Header/>
-
-      </div>
+          <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">Tienda de ropa</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     </div>
-  )
+  );
 }
 
 export default App;
